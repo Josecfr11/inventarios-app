2cc55ef6262e3500aad3dc20a1527dee5e7516bc
Josecfr11 <josecfr11@gmail.com>
2026-02-08 13:56:22 -0700

    Agrega libreria de primeng de estilos para componentes

---
+++ src/app/features/demo/button-demo/button-demo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-button-demo',
+  imports: [ButtonModule],
+  templateUrl: './button-demo.component.html',
+  standalone: true,
+  styleUrl: './button-demo.component.css'
+})
+export class ButtonDemoComponent {
+
+}
