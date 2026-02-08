1b7433038ceecb7f8b3c63605886a3da6f44821b
Josecfr11 <josecfr11@gmail.com>
2026-02-08 16:59:58 -0700

    agrega iconos de primeng y grid de bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Router, RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menubar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'inventarios-app';
+  items: MenuItem[] | undefined;
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.items = [
+      // {
+      //   label: 'Router',
+      //   icon: 'pi pi-palette',
+      //   items: [
+      //     {
+      //       label: 'Installation',
+      //       routerLink: '/installation'
+      //     },
+      //     {
+      //       label: 'Configuration',
+      //       routerLink: '/configuration'
+      //     }
+      //   ]
+      // },
+      {
+        label: 'Button',
+        icon: 'pi pi-link',
+        command: () => {
+          this.router.navigate(['/button']);
+        }
+      },
+      // {
+      //   label: 'External',
+      //   icon: 'pi pi-home',
+      //   items: [
+      //     {
+      //       label: 'Angular',
+      //       url: 'https://angular.io/'
+      //     },
+      //     {
+      //       label: 'Vite.js',
+      //       url: 'https://vitejs.dev/'
+      //     }
+      //   ]
+      // }
+    ];
+  }
+
 }
