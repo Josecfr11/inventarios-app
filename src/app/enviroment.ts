ce1074149b99edecbb9596fb4b15df65089cb24e
Josecfr11 <josecfr11@gmail.com>
2026-03-16 14:46:15 -0700

    fix(): devuelve rutas productivas

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  imagesUrl: 'http://localhost:8080'
+  apiUrl: 'https://inventcontrol-backend-1.onrender.com/api',
+  imagesUrl: 'https://inventcontrol-backend-1.onrender.com'
 };
